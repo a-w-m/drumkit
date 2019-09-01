2bc87a1713aea3027bb218b4ceb5839ee5bc0fc9
a-w-m <mamrey@gmail.com>
2019-09-01 16:37:22 -0400

    v1.1
    update transition event listener and cleaned up code

--- drumkit.js
+++ drumkit.js
@@ -1,108 +1,82 @@
 const drumKeys = document.getElementsByClassName("drum-pad");
 const volSlider = document.getElementById("volume-slider");
-const clips = document.getElementsByClassName("clip")
+const clips = document.getElementsByClassName("clip");
+const powerSlider = document.getElementById("power-slider");
 
-volSlider.addEventListener("change", ()=>{
-    Array.from(clips).forEach(clip => {
-        clip.volume =volSlider.value * .01
+function switchPower(e) {
+  if (powerSlider.style.transform == "") {
+    powerSlider.style.transform = "translateX(-5rem)";
+    powerSlider.innerHTML = "I";
+    display.style.opacity = ".1";
 
-})
-})
+    Array.from(drumKeys).forEach(key => {
+      key.style.opacity = ".1";
+      key.style.pointerEvents = "none";
+    });
+  } else {
+    powerSlider.style.transform = "";
+    powerSlider.innerHTML = "O";
+    display.style.opacity = "1";
 
+    Array.from(drumKeys).forEach(key => {
+      key.style.opacity = "1";
+      key.style.pointerEvents = "";
+    });
+  }
+}
 
-Array.from(drumKeys).forEach(key => {
-  let audio = key.childNodes[1]
-  key.onclick = function() {
-    if (audio){
-        audio.currentTime =0;
-
-        audio.parentElement.classList.toggle ("playing")
-
-                setTimeout(()=>{
-                    audio.parentElement.classList.toggle("playing")
-
-                }, 200)
-    audio.play();
-    display.innerHTML = key.id;
-    }
-  };
+Array.from(clips).forEach(clip => {
+  clip.volume = 0.5;
 });
 
-
-function KeyboardPress(){
-    let keyPress = event.key
-    let audio = document.getElementById(keyPress.toUpperCase())
-
-    if (powerSlider.style.transform !="translateX(-5rem)"){
-        if(audio){
-            //audio.clearTimeout()
-            audio.currentTime =0;
-        
-
-                audio.parentElement.classList.toggle ("playing")
-
-                setTimeout(()=>{
-                    audio.parentElement.classList.toggle("playing")
-
-                }, 200)
-
-                
-
-            
-
-            audio.play()
-            display.innerHTML =audio.parentElement.id
-            
-        }
-    }
-    else{
-        event.preventDefault();
-    }
-
-
+function changeVolume(e) {
+  Array.from(clips).forEach(clip => {
+    clip.volume = volSlider.value * 0.01;
+  });
 }
-document.addEventListener("keydown", KeyboardPress)
 
+function keyClick(e) {
+  let key = document.getElementById(e.srcElement.id);
+  let audio = document.getElementById(e.srcElement.innerText);
 
-
-const powerSlider = document.getElementById("power-slider")
-
-powerSlider.addEventListener('click', () => {
-    if (powerSlider.style.transform == "") {
-
-    powerSlider.style.transform = "translateX(-5rem)"
-    powerSlider.innerHTML = "I"
-    display.style.opacity = ".1"
-
-    
-
-    Array.from(drumKeys).forEach(key =>{ 
-        key.style.opacity = ".1"
-        key.style.pointerEvents = "none"
-        
-    })
-    
-      
+  if (audio) {
+    audio.currentTime = 0;
+    audio.parentElement.classList.toggle("playing");
+    audio.play();
+    display.innerHTML = key.id;
+  }
 }
-    else{
-        powerSlider.style.transform = ""
-        powerSlider.innerHTML= "O"
-        display.style.opacity = "1"
-    
-        
-        
-    Array.from(drumKeys).forEach(key =>{ 
-        key.style.opacity = "1"
-        key.style.pointerEvents = ""
 
-        
-    })
-    
-        
+function keyboardPress() {
+  let keyPress = event.key;
+  let audio = document.getElementById(keyPress.toUpperCase());
 
+  if (powerSlider.style.transform != "translateX(-5rem)") {
+    if (audio) {
+      audio.currentTime = 0;
+      audio.parentElement.classList.toggle("playing");
+      audio.play();
+      display.innerHTML = audio.parentElement.id;
     }
-})
+  } else {
+    event.preventDefault();
+  }
+}
 
+function removeTransition(e) {
+  this.classList.remove("playing");
+}
 
+powerSlider.addEventListener("click", switchPower);
 
+volSlider.addEventListener("change", changeVolume);
 
+Array.from(drumKeys).forEach(key => {
+  key.addEventListener("click", keyClick);
+});
+
+Array.from(drumKeys).forEach(key => {
+  key.addEventListener("transitionend", removeTransition);
+});
+
+document.addEventListener("keydown", keyboardPress);
