b43021ae15fad87a767c1b5150f76f24762abb8c
a-w-m <mamrey@gmail.com>
2019-09-03 14:29:48 -0400

    v1.2  mobile portrait/landscape responsive

--- drumkit.js
+++ drumkit.js
@@ -4,8 +4,10 @@ const clips = document.getElementsByClassName("clip");
 const powerSlider = document.getElementById("power-slider");
 
 function switchPower(e) {
+    /*This function is for the powerSlider 'click' event listener and controls the
+     'power switch' feature of the drumkit, managing the off/on transition of the switch */
   if (powerSlider.style.transform == "") {
-    powerSlider.style.transform = "translateX(-5rem)";
+    powerSlider.style.transform = "translateX(-100%)";
     powerSlider.innerHTML = "I";
     display.style.opacity = ".1";
 
@@ -30,12 +32,15 @@ Array.from(clips).forEach(clip => {
 });
 
 function changeVolume(e) {
+    /*This function changes the volume of the audio elements as the volume slider changes*/
   Array.from(clips).forEach(clip => {
     clip.volume = volSlider.value * 0.01;
   });
 }
 
 function keyClick(e) {
+    /* function for the key(drum-pad) 'click' event listener, plays the audio when key is clicked and changes the display text, 
+    allows for successive clicks with currentTime=0*/
   let key = document.getElementById(e.srcElement.id);
   let audio = document.getElementById(e.srcElement.innerText);
 
@@ -48,10 +53,12 @@ function keyClick(e) {
 }
 
 function keyboardPress() {
+ /* function for the document 'keydown' event listener, plays the audio when key is pressed and changes the display text, 
+    allows for successive pressed with currentTime=0*/
   let keyPress = event.key;
   let audio = document.getElementById(keyPress.toUpperCase());
 
-  if (powerSlider.style.transform != "translateX(-5rem)") {
+  if (powerSlider.style.transform != "translateX(-100%)") {
     if (audio) {
       audio.currentTime = 0;
       audio.parentElement.classList.toggle("playing");
@@ -64,6 +71,7 @@ function keyboardPress() {
 }
 
 function removeTransition(e) {
+    /*function that removes playing class on keys, used in transitionend event listener*/
   this.classList.remove("playing");
 }
 
